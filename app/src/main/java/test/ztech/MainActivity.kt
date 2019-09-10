55253a1f67a106233bfdf1d73077feb6c552c1b0
codedotman <dotunaboyeji@gmail.com>
2019-09-10 14:42:34 +0100

    wired up the buttom navigation with the fragment

--- app/src/main/java/test/ztech/MainActivity.kt
+++ app/src/main/java/test/ztech/MainActivity.kt
@@ -2,11 +2,36 @@ package test.ztech
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.view.MenuItem
+import test.ztech.views.ProfilePageFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        var fragment: Fragment? = null
+
+        when (p0.itemId) {
+            R.id.navigation_profile -> {
+                fragment = ProfilePageFragment()
+            }
+        }
+        return loadFragment(fragment)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun loadFragment(fragment: Fragment?): Boolean {
+        if (fragment != null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.home_container, fragment)
+                    .commit()
+            return true
+        }
+        return false
     }
 }
